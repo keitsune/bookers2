8d01ff15885eff49d68b69b8968fdc3393c93197
keitsune <tsune.kei.32821@gmail.com>
2021-10-30 14:25:38 +0000

    [Add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
   def show
@@ -11,5 +12,11 @@ class UsersController < ApplicationController
   end
 
   def update
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :Book, dependent: :destroy
+  attachment :profile_image
 end
